3a529d244d222bff6a1113649302c764c1b9d8aa
LuisAxel <axelnunez5@gmail.com>
2024-06-03 22:10:26 -0600

    FEAT: Automated dbs startup and shutdown
    - Created two different bash scripts to
      - Create PostgreSQL db, RiakKV cluster, MongoDB cluster and Neo4j cluster
      - Stop PostgreSQL db, RiakKV cluster, MongoDB cluster and Neo4j cluster

---
+++ app/dbs/start_dbs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Step 1: Almacena la ruta de donde se ejecuta el script
+SCRIPT_LOCATION=$(pwd)
+
+# Step 2: Inicia todas las dbs
+
+# Postgresql
+if [ ! "$(docker ps -q -f name=postgresql)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=postgresql)" ]; then
+        # cleanup
+        docker rm postgresql
+    fi
+    # Inicia el contenedor
+    docker run -d --name postgresql -p 5432:5432 luisaxel/postgresql:latest
+fi
+
+# Riak KV
+if [ ! "$(docker ps -q -f name=coordinator)" ] || [ ! "$(docker ps -q -f name=member)" ]; then
+    docker compose -f "${SCRIPT_LOCATION}/riakkv/docker-compose.yml" scale coordinator=1 member=3
+fi
+
+# MongoDB
+# Check if containers are running
+if [ ! "$(docker ps -q -f name=mongo1)" ]; then
+    docker compose -f "${SCRIPT_LOCATION}/mongodb/compose.yaml" up -d --wait
+fi
+
+# Neo4J
+# Check if containers are running
+if [ ! "$(docker ps -q -f name=neo4j1)" ]; then
+    docker compose -f "${SCRIPT_LOCATION}/neo4j/docker-compose.yaml" up -d
+fi
---
+++ app/dbs/stop_dbs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Step 1: Almacena la ruta de donde se ejecuta el script
+SCRIPT_LOCATION=$(pwd)
+
+# Step 2: Detén todos los contenedores
+
+# Postgresql
+if [ "$(docker ps -q -f name=postgresql)" ]; then
+    docker stop postgresql
+    docker rm postgresql
+fi
+
+# Riak KV
+# Check if containers are running and stop them
+if [ "$(docker ps -q -f name=coordinator)" ] || [ "$(docker ps -q -f name=member)" ]; then
+    docker compose -f "${SCRIPT_LOCATION}/riakkv/docker-compose.yml" down
+fi
+
+# MongoDB
+# Check if containers are running and stop them
+if [ "$(docker ps -q -f name=mongo1)" ]; then
+    docker compose -f "${SCRIPT_LOCATION}/mongodb/compose.yaml" down
+fi
+
+# Neo4J
+# Check if containers are running and stop them
+if [ "$(docker ps -q -f name=neo4j1)" ]; then
+    docker compose -f "${SCRIPT_LOCATION}/neo4j/docker-compose.yaml" down
+fi
